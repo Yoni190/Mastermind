7f6c667df0fe5f4d40b5bee2693516eb0771ecf6
Yoni190 <yfisuhu@gmail.com>
2024-08-20 17:41:38 +0300

    Greet player

--- lib/game.rb
+++ lib/game.rb
@@ -4,10 +4,17 @@ class Game
   attr_accessor :game_board, :player_guess
 
   def initialize
+    greet_player
     self.game_board = Board.new
     puts game_board.board
 
     prompt_player
+  end
+
+  def greet_player
+    puts "\t\s\s-----------------------------\n
+          |  Welcome to Mastermind!   |\n
+          -----------------------------"
   end
 
   def prompt_player
