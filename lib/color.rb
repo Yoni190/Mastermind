959e78a8b86a939b772f32c479c822f0d279a9b4
Yoni190 <yfisuhu@gmail.com>
2024-08-20 17:33:32 +0300

    Create a function to select 4 colors

--- lib/color.rb
+++ lib/color.rb
@@ -3,9 +3,10 @@ class Color
   COLORS = ["red", "blue", "yellow", "brown", "green", "purple"]
   def initialize
     self.random_colors = []
+    add_random_colors(random_colors)
   end
 
   def add_random_colors(array)
-    
+    array.push(COLORS.sample(4))
   end
 end
