62f01b2318f99d01bb81d4657729c9d66cea90f1
Yoni190 <yfisuhu@gmail.com>
2024-08-20 17:37:05 +0300

    Create a method to prompt player for guesses

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,17 @@
 require_relative 'board'
 
 class Game
-  attr_accessor :game_board
+  attr_accessor :game_board, :player_guess
 
   def initialize
     self.game_board = Board.new
     puts game_board.board
+
+    prompt_player
+  end
+
+  def prompt_player
+    puts "Hello, player. Please guess 4 random colors(put spaces between them)"
+    self.player_guess = gets.chomp.split
   end
 end
