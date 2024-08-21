7f6d000f560bfcffbd65ae45f1c5b3795df46e4b
Yoni190 <yfisuhu@gmail.com>
2024-08-21 10:54:48 +0300

    Create a computer class to choose random colors

---
+++ lib/computer.rb
@@ -0,0 +1,12 @@
+class Computer
+  attr_accessor :random
+  @@colors = ["red", "blue", "green", "yellow", "purple", "brown"]
+
+  def initialize
+    choose_four_colors(self.random)
+  end
+
+  def choose_four_colors(array)
+    array.push(sample(4))
+  end
+end
