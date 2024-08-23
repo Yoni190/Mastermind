b4c222805f913f323420fec485048ba9b98affa1
Yoni190 <yfisuhu@gmail.com>
2024-08-23 15:48:59 +0300

    Split choose four colors method so that it is not called when initialized

--- lib/code-maker.rb
+++ lib/code-maker.rb
@@ -3,14 +3,16 @@ class CodeMaker
   @@colors = ["red", "blue", "green", "yellow", "magenta", "cyan"]
 
   def initialize
-    choose_four_colors
+    if player_mode == "cb"
+      choose_four_colors
+    end
   end
 
   def choose_four_colors
-    if player_mode == "cb"
-      self.chosen_colors = @@colors.sample(4)
-    elsif player_mode == "cm"
-      self.chosen_colors = gets.chomp.split
-    end
+    self.chosen_colors = @@colors.sample(4)
+  end
+
+  def player_choose_colors
+    self.chosen_colors = gets.chomp.split
   end
 end
